a5f58ce744a3631075a5c4ca54a36e1fd5753a11
omdholariya007 <omdholariya2006@gmail.com>
2026-07-28 21:44:01 +0530

    Time: 50 ms (49.23%), Space: 54.1 MB (95.38%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
     string smallestPalindrome(string s) {
-        map<char,int>mp;
-        for(char c :s){
-            mp[c]++;
+        vector<int>v(26,0);
+        for(char c : s){
+            v[c-'a']++;
         }
-         s="";
-         pair<char,int>p ;
-        for(auto m = mp.begin() ; m != mp.end();m++){
-            char d = m->first;
-            int c = m->second;
-           if (c % 2 != 0) {
-                p = {d, c};
-            }
-            for(int i = 0 ; i < c/2 ;i++){
-                s+=d;
+        s="";
+        char t='0';
+        for(int i = 0 ; i < 26 ; i++){
+            if(v[i] %2 !=0) t = 'a'+i;
+            for(int j = 0 ; j <v[i]/2;j++ ){
+                char c = 'a'+i;
+                s+=c;
             }
         }
-        if(p.second %2 != 0) s+=p.first;
-        for(auto m = mp.rbegin() ; m != mp.rend();m++){
-            char d = m->first;
-            int c = m->second;
-            for(int i = 0 ; i < c/2 ;i++){
-                s+=d;
+        if(t != '0')s+=t;
+        for(int i = 25 ; i >=0 ; i--){
+            for(int j = 0 ; j <v[i]/2;j++ ){
+                char c = 'a'+i;
+                s+=c;
             }
         }
         return s;

805ac8bb3f85f3ead65252493a79e4b4ec359f3f
omdholariya007 <omdholariya2006@gmail.com>
2026-08-16 20:43:12 +0530

    Time: 316 ms (12.57%), Space: 134.3 MB (11.14%) - LeetHub

---
+++ 1738-find-kth-largest-xor-coordinate-value/1738-find-kth-largest-xor-coordinate-value.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int kthLargestValue(vector<vector<int>>& matrix, int k) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        vector<int> v;
+        vector<vector<int>> x(m + 1, vector<int>(n + 1, 0));
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                x[i][j] = matrix[i-1][j-1]
+                        ^ x[i-1][j]
+                        ^ x[i][j-1]
+                        ^ x[i-1][j-1];
+
+                v.push_back(x[i][j]);
+            }
+        }
+
+        sort(v.rbegin(), v.rend());
+
+        return v[k-1];
+    }
+};

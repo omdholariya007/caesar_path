851d00160baf001a9c37eeee4a097935fc1d948a
omdholariya007 <omdholariya2006@gmail.com>
2026-09-03 23:48:23 +0530

    Time: 0 ms (100%), Space: 115.6 MB (67.97%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int f = INT_MAX,s=INT_MAX;
+        for(int i = 0 ;i<nums.size();i++){
+            if(nums[i]<=f)f=nums[i];
+            else if(nums[i]<=s)s=nums[i];
+            else return true;
+        }
+        return false;
+    }
+};
